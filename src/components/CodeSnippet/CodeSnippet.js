a6f56a8a717b880d371a71ee116abebe7e371446
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-10-29 08:54:12 -0700

    Complete my solution

---
+++ src/components/CodeSnippet/CodeSnippet.js
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+
+function CodeSnippet({ children }) {
+  const [isSnippetShown, setIsSnippetShown] = React.useState(false);
+
+  if (isSnippetShown) {
+    return <>{children}</>;
+  }
+
+  return (
+    <div className='reveal'>
+      <button onClick={() => setIsSnippetShown(true)}>Reveal Content</button>
+    </div>
+  );
+}
+
+export default CodeSnippet;
